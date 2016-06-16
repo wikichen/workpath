fa00206ac0680702dd54208e1065ffd90b96236c
Jeremy Friedland <jeremyfriedland@gmail.com>
2016-06-15 22:24:16 -0700

    Add Tray expansion

--- Focus/DayViewController.swift
+++ Focus/DayViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class DayViewController: UIViewController {
 
+    @IBOutlet weak var trayView: UIView!
+    //Var for tray
+    var trayOriginalCenter: CGPoint!
+    var trayDownOffset: CGFloat!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //Set Tray View Values
+        trayDownOffset = 340
+        trayUp = trayView.center
+        trayDown = CGPoint(x: trayView.center.x, y: trayView.center.y + trayDownOffset)
+        
+        //Set Tray closed on Load
+        trayView.center = trayDown
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +38,62 @@ class DayViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func didPanTray(sender: UIPanGestureRecognizer) {
+        
+        let translation = sender.translationInView(view)
+        let velocity = sender.velocityInView(view)
+        
+        
+        if sender.state == UIGestureRecognizerState.Began {
+            
+            trayOriginalCenter = trayView.center
+            
+        } else if sender.state == UIGestureRecognizerState.Changed {
+            
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+            
+        } else if sender.state == UIGestureRecognizerState.Ended {
+            
+            if velocity.y > 0 {
+                // move down
+                
+                UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1,  options: [], animations: {
+                    () -> Void in self.trayView.center = self.trayDown
+                    }, completion: { (Bool) -> Void in
+                })
+                
+            } else {
+                // move up
+                UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1,  options: [], animations: {
+                    () -> Void in self.trayView.center = self.trayUp
+                    }, completion: { (Bool) -> Void in
+                })
+            }
+        }
+    }
+
+    
+    //Tapping on tray exposes tray
+    // Need to add logic for if task = 0 then tray cant be opened
+    @IBAction func didTapTray(sender: AnyObject) {
+        
+        if trayView.center == trayUp {
+            // move down
+            UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1,  options: [], animations: {
+                () -> Void in self.trayView.center = self.trayDown
+                }, completion: { (Bool) -> Void in
+            })
+            
+        } else {
+            // move up
+            UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1,  options: [], animations: {
+                () -> Void in self.trayView.center = self.trayUp
+                }, completion: { (Bool) -> Void in
+            })
+        }
+    }
+
+}
 
     /*
     // MARK: - Navigation
@@ -32,4 +105,3 @@ class DayViewController: UIViewController {
     }
     */
 
-}
